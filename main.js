477057c4b469eb32f5c33809896e4f56b7439432
Lynn Li <lynn.li@vechain.com>
2018-04-26 17:59:18 +0800

    fix

--- main.js
+++ main.js
@@ -8,7 +8,14 @@ function getWebusb(filters) {
         console.log(device.manufacturerName)
       })
     }
-    return navigator.usb.requestDevice({ filters: filters })
+    navigator.usb
+      .requestDevice({ filters: filters })
+      .then(device => {
+        console.log(device)
+      })
+      .catch(error => {
+        console.log(error)
+      })
   })
 }
 x.onclick = function() {
@@ -16,10 +23,4 @@ x.onclick = function() {
   const pid = document.getElementById('js-pid').value
 
   getWebusb([{ vendorId: vid, productId: pid }])
-    .then(device => {
-      console.log(device)
-    })
-    .catch(error => {
-      console.log(error)
-    })
 }
