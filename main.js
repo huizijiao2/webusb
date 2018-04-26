2293e1deded3a9874750d724a69fe51d3d0c8ce2
Lynn Li <lynn.li@vechain.com>
2018-04-26 17:26:16 +0800

    vid

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const x = document.getElementById('js-input')
 x.onclick = function() {
   window.navigator.usb
-    .requestDevice({ filters: [{ vendorId: 0x2341 }] })
+    .requestDevice({ filters: [{ vendorId: 0483 }] })
     .then(device => {
       console.log(device) // "Arduino Micro"
       console.log(device) // "Arduino LLC"
