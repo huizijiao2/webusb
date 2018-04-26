331fe15a66bf6e2031fadfe930dfde4d81c05870
Lynn Li <lynn.li@vechain.com>
2018-04-26 16:17:47 +0800

    1

---
+++ main.js
@@ -0,0 +1,12 @@
+const x = document.getElementById('js-input')
+x.onclick = function() {
+  navigator.usb
+    .requestDevice(params)
+    .then(device => {
+      console.log(device) // "Arduino Micro"
+      console.log(device) // "Arduino LLC"
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
