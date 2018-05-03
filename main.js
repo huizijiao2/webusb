74aa5ac1608eb20df47dcade3d01e059c960a8c9
Lynn Li <lynn.li@vechain.com>
2018-05-03 18:18:33 +0800

    test

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', event => {
 
       result = await device.controlTransferIn(getPort2, 9)
       const dataView = result.data
-      console.log('getPort2 port:', dataView.getUint8(2))
+      console.log('getPort2 port:', dataView.getInt8(6))
 
 
       result = await device.controlTransferOut(startPort)
